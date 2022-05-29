8781a56f612548e197bec093a556940dcaef86cf
Júlio Faria <juliano988>
2022-05-29 19:18:39 -0300

    update

--- api/db/model.js
+++ api/db/model.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_DB_URI);
+mongoose.connect(process.env.MONGO_DB_URI); 
 
 const UserSchema = new mongoose.Schema({
   name: String,
